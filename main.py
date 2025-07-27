8e975272a7d9db1da290144c1b7f9eab01ac021f
ArjunnnnGupta <146002248+ArjunnnnGupta@users.noreply.github.com>
2025-07-27 13:25:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from scheduler import start_scheduler
 app = Flask(__name__)
 app.register_blueprint(bp)
 
-start_scheduler()
+@app.before_first_request
+def activate_scheduler():
+    start_scheduler()
 
-# This part runs only when executing locally (e.g., python main.py)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000, debug=True)
