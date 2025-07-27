5fbefa43ccc55720d3167444f1feb291db8ee307
ArjunnnnGupta <146002248+ArjunnnnGupta@users.noreply.github.com>
2025-07-27 14:38:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from scheduler import start_scheduler
 app = Flask(__name__)
 app.register_blueprint(bp)
 
-@app.before_request
-def activate_scheduler():
-    start_scheduler()
+# Start scheduler once
+start_scheduler()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render's port if available
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
